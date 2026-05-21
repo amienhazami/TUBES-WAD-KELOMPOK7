1c7db9ff6d4499a70db5f3a8c7d06a7bbd9f0210
amienhazami <amienhazami@gmail.com>
2026-05-21 14:15:06 +0700

    Configure trustProxies and dynamic SQLite fallback for seamless Railway deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,10 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'role' => CheckRole::class,
         ]);
-
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
